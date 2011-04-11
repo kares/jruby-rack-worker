39c73e232a04b4ab9dc723b9101293b8b21e3c7a
kares <self@kares.org>
2011-04-11 19:42:35 +0200

    a better worker name for DJ

--- src/main/ruby/delayed/start_worker.rb
+++ src/main/ruby/delayed/start_worker.rb
@@ -1,6 +1,8 @@
 begin
   require 'delayed/jruby_worker'
-  Delayed::JRubyWorker.new(:quiet => true).start
+  worker = Delayed::JRubyWorker.new(:quiet => true)
+  worker.name = "host:#{Socket.gethostname} thread:#{java.lang.Thread.currentThread.getName}"
+  worker.start
 rescue => e
   Rails.logger.fatal(e) if defined?(Rails.logger)
   STDERR.puts e.message
